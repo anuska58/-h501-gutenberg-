763b7f710b5b061565ea135cc04b7accf9647fb7
anuska58 <aanuska58@gmail.com>
2025-09-23 15:29:56 -0400

    fix name

--- tt_gutenberg/data_loader.py
+++ tt_gutenberg/data_loader.py
@@ -1,5 +1,8 @@
 import pandas as pd
-def data_loader():
+def load_data():
+    """
+    Load the TidyTuesday Gutenberg dataset using URL.
+    """
     url='https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-06-03/gutenberg_authors.csv'
     df=pd.read_csv(url)
     return df
